97d51e86c6234aaaf6a2db3ea61a124c26f4d288
cmugabo <c.mugabo@alustudent.com>
2022-11-01 19:27:22 +0200

    added file

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -6,4 +6,4 @@ def search_replace(my_list, search, replace):
     for i in range(0, len(new_list)):
         if search == new_list[i]:
             new_list[i] = replace
-    return new_lists
+    return new_list
