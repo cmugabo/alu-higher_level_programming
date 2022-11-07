85db99f2728056e528f4f275ae319c188767c2c9
cmugabo <c.mugabo@alustudent.com>
2022-11-07 20:05:02 +0200

    added

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new = [0] * list_length
@@ -15,4 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             pass
     return new
+
 
