5be6c9daf5f9c89ab1a1c75d49d0ec2305b70334
cmugabo <c.mugabo@alustudent.com>
2022-11-07 20:02:00 +0200

    added

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new = [0] * list_length
@@ -13,4 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             pass
-    return 
+    return new
+
