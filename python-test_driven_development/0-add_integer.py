d27f0b9cdb69322c766047884db4e3162fd6aa6c
cmugabo <c.mugabo@alustudent.com>
2022-12-12 10:53:34 +0200

    added

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,7 +6,7 @@
 
 
 def add_integer(a=0, b=98):
-    """ function adds integers """
+    """ function that sums upintegers """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
